2168fe817119307965794ff8ad442dd9ec2627e5
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-03-21 20:08:29 +0100

    add failing test for creating payment

--- app/app_test.go
+++ app/app_test.go
@@ -1,7 +1,13 @@
 package app
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -9,4 +15,35 @@ func TestCreateApp(t *testing.T) {
 	app := CreateApp()
 
 	assert.NotNil(t, app.Router)
+}
+
+func TestCreatesPayment(t *testing.T) {
+	app := CreateApp()
+
+	response := app.createPayment()
+
+	assert.Equal(t, http.StatusCreated, response.Code)
+
+	var responseBody map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &responseBody)
+
+	assert.Equal(t, responseBody["id"], "4ee3a8d8-ca7b-4290-a52c-dd5b6165ec43")
+}
+
+func (app *App) createPayment() *httptest.ResponseRecorder {
+	payload, err := readTestFile("create-payment-1.json")
+	if err != nil {
+		fmt.Println("Error loading data")
+		fmt.Println(err)
+	}
+
+	req, _ := http.NewRequest("POST", "/payments", bytes.NewBuffer(payload))
+	response := httptest.NewRecorder()
+	app.Router.ServeHTTP(response, req)
+
+	return response
+}
+
+func readTestFile(filename string) ([]byte, error) {
+	return ioutil.ReadFile(fmt.Sprintf("../test_data/%s", filename))
 }
