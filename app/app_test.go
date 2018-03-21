11809b9aa625f7891f51c1ffa145809a0a932aa0
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-03-21 23:50:24 +0100

    add failing test for getting all payments

--- app/app_test.go
+++ app/app_test.go
@@ -62,6 +62,29 @@ func TestGetsPayment(t *testing.T) {
 	assert.Equal(t, expected, responseBody)
 }
 
+func TestGetAllPayments(t *testing.T) {
+	paymentsRepository := payments.PaymentsRepository{}
+	app := CreateApp(&paymentsRepository)
+	app.createPayment("create-payment-1_request.json")
+	app.createPayment("create-payment-2_request.json")
+
+	response := app.getAllPayments()
+
+	assert.Equal(t, http.StatusOK, response.Code)
+
+	var responseBody map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &responseBody)
+
+	expectedJson, err := readTestFile("all-payments_response.json")
+	if err != nil {
+		fmt.Println("Error loading data")
+		fmt.Println(err)
+	}
+	var expected map[string]interface{}
+	json.Unmarshal(expectedJson, &expected)
+	assert.Equal(t, expected, responseBody)
+}
+
 func (app *App) createPayment(filename string) *httptest.ResponseRecorder {
 	payload, err := readTestFile(filename)
 	if err != nil {
@@ -78,6 +101,14 @@ func (app *App) createPayment(filename string) *httptest.ResponseRecorder {
 
 func (app *App) getPayment() *httptest.ResponseRecorder {
 	req, _ := http.NewRequest("GET", "/payments/4ee3a8d8-ca7b-4290-a52c-dd5b6165ec43", nil)
+	response := httptest.NewRecorder()
+	app.Router.Router.ServeHTTP(response, req)
+
+	return response
+}
+
+func (app *App) getAllPayments() *httptest.ResponseRecorder {
+	req, _ := http.NewRequest("GET", "/payments", nil)
 	response := httptest.NewRecorder()
 	app.Router.Router.ServeHTTP(response, req)
 
