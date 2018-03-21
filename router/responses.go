e39521172bf44d439ef1eff55b9aa5fc63be44c0
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-03-21 22:00:02 +0100

    Revert "refactor: extract generic payload"
    This reverts commit 804d1cf8d84354b44f541d30c9e1ebc4bb7e37db.
    
    Premature abstraction

--- router/responses.go
+++
@@ -1,9 +0,0 @@
-package router
-
-type Payload struct {
-	Id             string      `json:"id"`
-	Type           string      `json:"type"`
-	Version        int         `json:"version"`
-	OrganisationId string      `json:"organisation_id"`
-	Attributes     interface{} `json:"attributes"`
-}
--- router/router.go
+++ router/router.go
@@ -12,6 +12,14 @@ type Router struct {
 	Router *mux.Router
 }
 
+type PaymentPayload struct {
+	Id             string            `json:"id"`
+	Type           string            `json:"type"`
+	Version        int               `json:"version"`
+	OrganisationId string            `json:"organisation_id"`
+	Payment        *payments.Payment `json:"attributes"`
+}
+
 func NewRouter(paymentsRepository *payments.PaymentsRepository) *Router {
 	paymentsService := payments.NewPaymentsService(paymentsRepository)
 
@@ -45,11 +53,11 @@ func CreatePaymentHandler(paymentsService *payments.PaymentsService) func(w http
 }
 
 func createPaymentPayload(payment *payments.Payment) ([]byte, error) {
-	return json.Marshal(Payload{
+	return json.Marshal(PaymentPayload{
 		Id:             payment.Id,
 		Type:           "Payment",
 		Version:        0,
 		OrganisationId: "743d5b63-8e6f-432e-a8fa-c5d8d2ee5fcb",
-		Attributes:     payment,
+		Payment:        payment,
 	})
 }
