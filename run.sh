4205e1ac1a1ac4627de04666365e73b3b94023cb
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-03-22 11:54:55 +0100

    make scripts exit on failure

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 docker build -t payments-server .
 docker run --rm -t -i payments-server
--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 docker build -f Dockerfile-tests -t payments-server-tests .
 docker run --rm -t -i payments-server-tests
